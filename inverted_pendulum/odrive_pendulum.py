264bab649ecfff52031a612f5727b1e1e92f42bc
Michael Greer <michaelryangreer@gmail.com>
2021-03-15 12:04:27 -0600

    Added markdown to nullspace notebook Small changes to inverted pendulum sim and control

--- inverted_pendulum/odrive_pendulum.py
+++ inverted_pendulum/odrive_pendulum.py
@@ -43,6 +43,8 @@ wait_for_odrive()
 
 odrv0.axis1.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
 
+wait_for_odrive()
+
 print("Calibration complete")
 
 
@@ -50,22 +52,26 @@ for i in range(10):
 
     time.sleep(0.1)
     print(odrv0.axis1.encoder.pos_estimate)
+    print(odrv0.axis0.encoder.pos_estimate)
 
 wait_for_odrive()
 
-#odrv0.axis1.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
-#odrv0.axis1.controller.input_velocity = 0.0
+odrv0.axis1.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
+odrv0.axis1.controller.input_vel = 0.0
 
-odrv0.axis1.controller.config.control_mode = CONTROL_MODE_TORQUE_CONTROL
-odrv0.axis1.controller.input_torque = 0.0
+#odrv0.axis1.controller.config.control_mode = CONTROL_MODE_TORQUE_CONTROL
+#odrv0.axis1.controller.input_torque = 0.0
 
 odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
 for i in np.linspace(0, 8*np.pi, 1400):
 
-    tau = 0.01 if np.sin(i) > 0 else -0.01
+    tau = 0.04 if np.sin(0.8*i) > 0 else -0.04
+
+    print("Commanded torque is: {}".format(tau))
 
     odrv0.axis1.controller.input_torque = tau
+    odrv0.axis1.controller.input_vel = tau
     time.sleep(0.001)
 
 odrv0.axis1.requested_state = AXIS_STATE_IDLE
